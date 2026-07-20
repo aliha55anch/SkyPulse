38e936e6112ef430be801ab89bc96bd8e5646204
aliha55anch <chali37125@gmail.com>
2026-07-20 11:54:46 +0500

    initial commit

---
+++ src/components/AirConditions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AirConditions() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AirConditions
---
+++ src/components/TodaysForecast.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodaysForecast() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TodaysForecast
